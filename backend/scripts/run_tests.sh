25cfb23d0ccff0b5bc987a599c7b238af13b3cdc
emmanuel.ochade <emmanuel.ochade@interswitchng.com>
2026-04-28 08:25:57 +0100

    updated test scripts

--- backend/scripts/run_tests.sh
+++ backend/scripts/run_tests.sh
@@ -7,5 +7,4 @@ python -m coverage report
 
 # From backend/scripts directory, run this script to execute all tests and see the coverage report. Make sure you have the virtual environment set up and the necessary dependencies installed before running this script.
 
-# chmod +x run_tests.sh
-# ./run_tests.sh
+# bash run_tests.sh
