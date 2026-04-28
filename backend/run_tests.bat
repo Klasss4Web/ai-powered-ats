31b2fbb5f72dd9d2297b63623a4186971cb9b8f8
emmanuel.ochade <emmanuel.ochade@interswitchng.com>
2026-04-28 08:38:59 +0100

    updated test scripts

--- backend/run_tests.bat
+++
@@ -1,6 +0,0 @@
-
-
-# From backend directory, run this script to execute all tests and see the coverage report. Make sure you have the virtual environment set up and the necessary dependencies installed before running this script.
-
-# chmod +x run_tests.sh
-# ./run_tests.sh
--- backend/run_tests.sh
+++
@@ -1,5 +0,0 @@
-
-
-# From backend directory, run this script to execute all tests and see the coverage report. Make sure you have the virtual environment set up and the necessary dependencies installed before running this script.
-
-#  bash run_tests.sh
